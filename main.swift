9ce6fa94505f19fb0873837ac2d9f5535f6d1f29
Sizov Denis <sv_skatch@bk.ru>
2022-01-02 16:19:20 +0300

    arrCount, findMax, binarySearch recursion algorithms

--- main.swift
+++ main.swift
@@ -7,3 +7,51 @@
 
 import Foundation
 
+func arrCount (_ arr: Array<Int>) -> Int {
+	var arr = arr
+	if arr.isEmpty {
+		return 0
+	}
+	
+	arr.removeFirst()
+	return 1 + arrCount(arr)
+}
+
+func findMax(_ arr: Array<Int>, currentMax: Int = 0) -> Int {
+	var arr = arr
+	if arr.isEmpty {
+		return currentMax
+	}
+	
+	let currentElement = arr.removeFirst()
+	
+	if currentMax > currentElement {
+		return findMax(arr, currentMax: currentMax)
+	} else {
+		return findMax(arr, currentMax: currentElement)
+	}
+}
+
+func binarySearchRec(_ arr: Array<Int>, minIndex: Int, maxIndex: Int, element: Int) -> Bool {
+	
+	let index = ( maxIndex + minIndex ) / 2
+	let currentElement = arr[index]
+	
+	if currentElement == element {
+		return true
+	} else if minIndex == maxIndex {
+		return false
+	}
+	
+	if element < currentElement {
+		return binarySearchRec(arr, minIndex: minIndex, maxIndex: index - 1, element: element)
+	} else {
+		return binarySearchRec(arr, minIndex: index + 1, maxIndex: maxIndex, element: element)
+	}
+}
+
+func checkArray(_ array: Array<Int>, for element: Int) -> Bool {
+	return binarySearchRec(array, minIndex: 0, maxIndex: array.count - 1, element: element)
+}
+
+print(checkArray([2, 5, 7, 11, 13, 17], for: 22))
